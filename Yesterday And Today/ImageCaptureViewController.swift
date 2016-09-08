10d5c988dab5268ee5ebb84878b64c1d78ed7d32
Tim <tim.bathgate@gmail.com>
2016-09-08 13:55:37 +1200

    Add AvCaptureSession

--- Yesterday And Today/ImageCaptureViewController.swift
+++ Yesterday And Today/ImageCaptureViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ImageCaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIScrollViewDelegate {
 
     @IBOutlet weak var pastImage: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var cameraView: UIView!
+    
+    let captureSession = AVCaptureSession()
+    let stillImageOutput = AVCaptureStillImageOutput()
+    var error: NSError?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBarHidden = true
+        pastImage.contentMode = .ScaleAspectFill
 
         scrollView.minimumZoomScale = 1.0
         scrollView.maximumZoomScale = 6.0
+        
+        let devices = AVCaptureDevice.devices().filter{ $0.hasMediaType(AVMediaTypeVideo) && $0.position == AVCaptureDevicePosition.Back }
+        if let captureDevice = devices.first as? AVCaptureDevice  {
+            
+            do {
+                try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+            } catch {
+                print ("MRH")
+            }
+            
+            captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+            captureSession.startRunning()
+            stillImageOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
+            if captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addOutput(stillImageOutput)
+            }
+            if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
+                previewLayer.bounds = view.bounds
+                previewLayer.position = CGPointMake(view.bounds.midX, view.bounds.midY)
+                previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                let cameraPreview = UIView(frame: CGRectMake(0.0, 0.0, cameraView.bounds.size.width, cameraView.bounds.size.height))
+                cameraPreview.layer.addSublayer(previewLayer)
+                cameraView.addSubview(cameraPreview)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
